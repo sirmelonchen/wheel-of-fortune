6916d98a8fec0ec8438e6320bed16199aa41eeca
sirmelonchen <beug.kon@outlook.de>
2024-05-22 20:40:15 +0200

    edited main.c | added file read, added file create, added main

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(){
+
+void filecreate() {
+    FILE *fptr;
+    fptr = fopen("wheel.txt", "w");
+    fclose(fptr);
+}
+
+
+void fileread(){
+    char myString[100];
+    FILE *fptr;
+    fptr = fopen("wheel.txt", "r");
+    if (fptr == NULL)
+    {
+        printf("Wir können die Datei \"wheel.txt\" nicht öffnen.\n Es wird eine neue Datei erstellt.\n Bitte das Programm neustarten." );
+        filecreate();
+        exit(0);
+    }else{
+        while(fgets(myString, 100, fptr)) {
+            printf("%s\n", myString);
+  }
+    }
+    
+}
+
+
+
+int main() {
+
     printf("Hello World!");
+    fileread();
     return 0;
 }
+

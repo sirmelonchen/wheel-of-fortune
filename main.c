cd96772c7d3ee117f2b387302ec6e0dc03d144b5
sirmelonchen <beug.kon@outlook.de>
2024-05-24 20:41:02 +0200

    changed main.c | added random integration

--- main.c
+++ main.c
@@ -9,12 +9,26 @@ void filecreate() {
     fclose(fptr);
 }
 
+int random_line(int lines){
+    lines++;
+    int r = rand() % lines;
+    printf("%d", r);
+    return lines;
+}
 
-void fileread(){
+
+void count_lines(){
   // count the number of lines in the file called filename                                    
   FILE *fp = fopen("wheel.txt","r");
   int ch=0;
   int lines=0;
+
+  if(fp == NULL){
+    filecreate();
+    printf("Du musst das Programm neu starten");
+    exit(0);
+  }
+  
 
 
   lines++;
@@ -25,13 +39,19 @@ void fileread(){
     }
   fclose(fp);
   printf("%d", lines);
+  int printline = random_line(lines);
+
+
     
 }
 
 
 
+
+
+
 int main() {
-    fileread();
+    count_lines();
     return 0;
 }
 
